2f2339ae0fd02abc9e1709d4db4c139381269eb2
unicorncupcake13 <samantha.broverman@gmail.com>
2024-09-17 09:20:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 write_a_sentence = input ("write a sentence")
-if write_a_sentence == "
+if write_a_sentence == " I like the color purple":
+    print("correct")
+elif write_a_sentence == "my name is Misha":
     print("correct")
 else:
-    print("incorrect")
+    print("incorrect") 
+
+x = 
