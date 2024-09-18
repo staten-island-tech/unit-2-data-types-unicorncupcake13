d53ac3d7d8d2b194f14182d71f888087a2d09e51
unicorncupcake13 <samantha.broverman@gmail.com>
2024-09-18 09:22:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-write_a_sentence = input ("write a sentence")
-if write_a_sentence == " I like the color purple":
-    print("correct")
-elif write_a_sentence == "my name is Misha":
-    print("correct")
-else:
-    print("incorrect") 
+#write_a_sentence = input ("write a sentence")
+#if write_a_sentence == "I like the color purple":
+    #print("correct")
+    #print("5 words")
+#elif write_a_sentence == "my name is Misha":
+    #print("correct")
+    #print("4 words")
+#else:
+    #print("incorrect") 
+    #print("1 word")
 
-x = 
+int == int(input ("input a number"))
+if int % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+bill_service ==
+
